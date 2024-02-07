74894bae23668a11ac8bb2c19545344e31243482
Meera A <mirunaalini.alagarajah@gmail.com>
2024-02-07 11:59:58 +0000

    images for properties added

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -7,7 +7,9 @@ import detached from '../../src/images/house3.jpg';
 import flat from '../../src/images/house4.jpg';
 import cottage from '../../src/images/cottage.jpg';
 import '../../src/styles/properties.scss';
- import NavBar from './Navbar';
+import NavBar from './Navbar';
+import endterrace from '../../src/images/endterrace.jpg';
+import bungalow from '../../src/images/bungalow.jpg';
 
 const Properties = () => {
 
@@ -44,30 +46,37 @@ const Properties = () => {
 
 
     return (
-        <div className="properties_page">
+        <div className="properties-page">
             <NavBar/>
             
             
-            <h1 className="title_page">List of Properties</h1>
-            <h2>main page</h2>
-            <div className='properties data'>
+            <h1 className="title-page">List of Properties</h1>
+           
+            <div className='properties-data'>
                 
 
                 {
-                    properties.map((property,i) => (
+                    properties.map((property, i) => (
+                        
                         <ul key={i}>
-                            <li>Property:{property.title}</li>
-                            <li>Type:{property.type}</li>
-                            <li>City:{property.city}</li>
-                            <li>Listed price:{property.price}</li>
-                            <li>Bedrooms{property.bedrooms}</li>
-                            <li>Bathrooms:{property.bathrooms}</li>
-                            <li>Contact for more info:{property.email}</li>
+                            <li>
+                                <h3>Property</h3>
+                            </li>
+                            <li>Property: {property.title}</li>
+                            <li>Type: {property.type}</li>
+                            <li>City: {property.city}</li>
+                            <li>Listed price: {property.price}</li>
+                            <li>Bedrooms: {property.bedrooms}</li>
+                            <li>Bathrooms: {property.bathrooms}</li>
+                            <li>Contact for more info: {property.email}</li>
+                      
                             {property.type === 'detached' && <img src={detached} alt="detached house icon" />}
                             {property.type === 'semi-detached' && <img src={semiDetached} alt="semi detached house icon" />}
-                            {property.type === 'terraced' && <img src={terraced} alt="terraced house icon" />}
+                            {property.type === 'end of terrace' && <img src={endterrace} alt="terraced house icon" />}
                             {property.type === 'flat' && <img src={flat} alt="flat icon" />}
-                            {property.type === 'bungalow' && <img src={ cottage} alt="cottage icon" />}
+                            {property.type === 'cottage' && <img src={cottage} alt="cottage icon" />}
+                            {property.type === 'bungalow' && <img src={bungalow} alt="bungalow icon" />}
+                            {property.type === 'terraced' && <img src={ terraced} alt="end terraced house icon" />}
                             
                         </ul>
                     ))
